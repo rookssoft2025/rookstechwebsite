11dcede0a1566ed575a192888c09e7a2acdcc872
shyju <shyju.rooks@gmail.com>
2025-09-10 10:00:11 +0530

    local commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Home from "./pages/Home";
+// import HomePage from "./pages/HomePage"; // create later if needed
+// import AboutPage from "./pages/AboutPage";
+
+function App() {
+  return (
+    <>
+     
+      <Routes>
+        <Route path="/" element={ <Home/>} />
+        {/* <Route path="/about" element={<AboutPage />} /> */}
+        
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss({
+      config: {
+        theme: {
+          extend: {
+            keyframes: {
+              fadeInLeft: {
+                "0%": { opacity: "0", transform: "translateX(-50px)" },
+                "100%": { opacity: "1", transform: "translateX(0)" },
+              },
+              fadeInRight: {
+                "0%": { opacity: "0", transform: "translateX(50px)" },
+                "100%": { opacity: "1", transform: "translateX(0)" },
+              },
+            },
+          },
+        },
+      },
+    }),
+  ],
+});
